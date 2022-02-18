0d8aa26255101c89628df8e804bba2618ce672fa
IBrickhouse <brickhoi@oregonstate.edu>
2022-02-17 20:10:13 -0800

    Create a blank window with circle
    Create a window with a white screen and red circle

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pygame
 pygame.init()  
 scr = pygame.display.set_mode((600,500))  
 pygame.display.set_caption('Pygame Window')
-done = False  
-while not done:  
+running = True  
+while running:  
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
-            done = True  
-pygame.display.flip()  
+            running = False  
+    scr.fill((255,255,255))
+    pygame.draw.circle(scr, (200, 0, 0), (250, 250), 80)
+    pygame.display.flip()  
+pygame.quit()
